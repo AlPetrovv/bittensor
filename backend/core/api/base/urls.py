4a94881c0c002bfff7d9ffca9b1d0c815a2b9721
alelsandr <sanya10081999@gmail.com>
2025-01-04 11:12:23 +0700

    fix: delete redoc from docs and add v1 for docs

--- backend/core/api/base/urls.py
+++ backend/core/api/base/urls.py
@@ -3,15 +3,10 @@ from drf_spectacular import views
 from django.urls import path
 
 urlpatterns = [
-    path("api/schema/", views.SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/v1/", views.SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "api/schema/v1/swagger-ui/",
         views.SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-    ),
-    path(
-        "api/schema/redoc/",
-        views.SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
     ),
 ]
